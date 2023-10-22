e6ae511ce846bc40e0ca71d8fb4d95d94d80a31f
golam mostafa <gmnayeem25@gmail.com>
2023-10-23 01:38:20 +0600

    add client

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-center font-bold text-lg mt-4'>Blockchain Implementaion</h1>
+    </div>
+  )
+}
+
+export default App
