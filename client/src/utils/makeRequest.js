59e488dd37c6dd1ae336cb7108eeecb858a1f5b7
golam mostafa <gmnayeem25@gmail.com>
2023-10-31 01:54:08 +0600

    fix problems

--- client/src/utils/makeRequest.js
+++ client/src/utils/makeRequest.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const REACT_APP_API_URL = 'http://localhost:4500/api';
-const REACT_APP_API_URL = 'https://blockchain-project-api.vercel.app/api';
+const REACT_APP_API_URL = 'http://localhost:4500/api';
+// const REACT_APP_API_URL = 'https://blockchain-project-api.vercel.app/api';
 
 export const publicRequest = axios.create({
     baseURL: REACT_APP_API_URL
