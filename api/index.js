f3f3fcc46242b19b94a46b18ef453151b11cfb40
golam mostafa <gmnayeem25@gmail.com>
2023-10-31 00:25:28 +0600

    some correction

--- api/index.js
+++ api/index.js
@@ -2,5 +2,5 @@ const app = require('./app');
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-    console.log(`Server is running successfull`);
+    console.log(`Server is running successfully`);
 });
