0bd316914a6d1dd1647529ca60f702bc5f256de5
golam mostafa <gmnayeem25@gmail.com>
2023-11-03 22:29:28 +0600

    add title

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="w-full bg-gray-400 text-white py-4 text-center">
-      <p>All Rights Reserved by &copy; BlockChain b</p>
+      <p>All Rights Reserved by &copy; Product Authenticity</p>
     </div>
   );
 };
